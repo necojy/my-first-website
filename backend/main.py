aaf8fdf61798232ffa37142e9e25ec01edb454ff
necojy <jim2323098@gmail.com>
2026-02-15 22:34:28 +0800

    to docker

--- backend/main.py
+++ backend/main.py
@@ -19,32 +19,18 @@ app.add_middleware(
 
 @app.get("/api/open_browser")
 def test_browser():
-    # 1. 設定瀏覽器的啟動選項
     chrome_options = Options()
-    
-    # 【加入更嚴格的雲端防當機參數】
-    chrome_options.add_argument("--headless=new") # 使用新版的無頭模式 (推薦)
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu") # 雲端沒有顯示卡，必須禁用
-    chrome_options.add_argument("--window-size=1920,1080") # 給定一個虛擬的螢幕解析度
-    chrome_options.add_argument("--remote-debugging-port=9222") # 解決 DevToolsActivePort 閃退的關鍵！
-    chrome_options.add_argument("--disable-extensions") # 停用擴充功能以節省資源
-
-    # 2. 智慧判斷環境，告訴程式去哪裡找瀏覽器
-    if os.path.exists("./chrome-linux64/chrome"):
-        # 👉 情況 A：如果在 Render 雲端
-        chrome_options.binary_location = "./chrome-linux64/chrome"
-        service = Service("./chromedriver-linux64/chromedriver")
-    else:
-        # 👉 情況 B：如果在你的本地端電腦
-        service = Service(ChromeDriverManager().install())
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
 
     try:
-        # 3. 啟動瀏覽器
+        # Docker 已經幫我們把系統環境裝好了，直接用最乾淨的寫法！
+        service = Service(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
         
-        # 4. 命令瀏覽器前往指定網址
         driver.get("https://www.youtube.com/")
         time.sleep(3) 
         page_title = driver.title
