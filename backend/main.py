3b3a62f7defcb86a590c3c7610d6637e2d26f7a7
necojy <jim2323098@gmail.com>
2026-02-24 12:19:21 +0800

    test

--- backend/main.py
+++ backend/main.py
@@ -85,20 +85,20 @@ def test_browser(action: str = "both"):
                 driver.execute_script("window.stop();")
             except Exception:
                 pass
-            time.sleep(2)
+            time.sleep(10)
     
             username_input = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='會員卡號/電子郵件信箱/手機號碼']")))
             username_input.clear()
             username_input.send_keys(acc["user"]) 
-            time.sleep(1)
+            time.sleep(10)
 
             password_input = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password']")))
             password_input.clear()
             password_input.send_keys(acc["pass"]) 
-            time.sleep(1)
+            time.sleep(10)
             
             password_input.send_keys(Keys.RETURN)
-            time.sleep(8) 
+            time.sleep(12) 
             # ==========================================
             # 🌟 2. 任務分流：如果選擇抓取優惠卷，或兩者皆抓
             # ==========================================
@@ -109,11 +109,11 @@ def test_browser(action: str = "both"):
                         EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/my-account/ecouponsEvouchers')]"))
                     )
                     driver.execute_script("arguments[0].click();", coupon_tab)
-                    time.sleep(4) 
+                    time.sleep(10) 
                     
                     try:
                         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "e2-my-account-current-coupon")))
-                        time.sleep(2) 
+                        time.sleep(10) 
                     except:
                         pass
 
@@ -170,14 +170,14 @@ def test_browser(action: str = "both"):
                             EC.presence_of_element_located((By.XPATH, "//a[contains(@href, '/my-account/orders')]"))
                         )
                         driver.execute_script("arguments[0].click();", orders_menu)
-                        time.sleep(6)
+                        time.sleep(10)
 
                     store_records_tab = wait.until(EC.presence_of_element_located((By.XPATH, "//li[contains(@class,'nav-item') and contains(.,'門市交易紀錄')]")))
                     driver.execute_script("arguments[0].click();", store_records_tab)
-                    time.sleep(5) 
+                    time.sleep(10) 
 
                     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.orders-containers")))
-                    time.sleep(4) 
+                    time.sleep(10) 
 
                     page_html = driver.page_source
                     soup = BeautifulSoup(page_html, 'html.parser')
